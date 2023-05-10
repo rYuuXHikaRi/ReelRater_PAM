9452a73f3761caece14ad1df0a4e0ab08f509213
Edo Sani <sani140402@gmail.com>
2023-05-10 11:08:16 +0700

    update

--- components/screens/detail_screen.js
+++ components/screens/detail_screen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {StyleSheet,Text,View,Image,Pressable,} from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
+
 const DetailPage = ({ navigation }) => {
 
     return (
@@ -11,22 +12,24 @@ const DetailPage = ({ navigation }) => {
             Detail
           </Text>
         </View>
-        <View style={styles.BoxGambar}></View>
+        <View style={styles.BoxGambar}>
+          <Image source={require("../../assets/buya_hamka.png")} style={styles.gambar} />
+        </View>
         <Text style={styles.judul}>
-          Judul Film
+          Buya Hamka
         </Text>
         <View style={styles.BoxJudul}>
           <Text style={styles.cast}>
-            Cast:
+            Cast: Vino Bastian, Laudya Cynthia Bella, Donny Damara, more
           </Text>
           <Text style={styles.from}>
-            Asal Film:
+            Indonesia Movies
           </Text>
           <Text style={styles.rilis}>
-            Tanggal Rilis:
+            18 April 2023
           </Text>
           <Text style={styles.durasi}>
-            Durasi:
+            1h 46m
           </Text>
         </View>
         <Text style={styles.genre}>
@@ -34,13 +37,24 @@ const DetailPage = ({ navigation }) => {
         </Text>
         <View style={styles.BoxJenis}>
           <Text style={styles.jenis}>
-            Action
+            Biografi
+          </Text>
+        </View>
+        <View style={styles.BoxJenis2}>
+          <Text style={styles.jenis2}>
+            Drama
           </Text>
         </View>
         <Text style={styles.sinopsis}>
           Sinopsis
         </Text>
-        <View style={styles.BoxSinopsis}></View>
+        <View style={styles.BoxSinopsis}>
+          <Text style={styles.spoiler}>
+          Film ini lebih berfokus pada kehidupan Abdul Malik Karim Amrullah alias Buya Hamka saat aktif menjadi pengurus Muhammadiyah Makassar. 
+          Selain itu, kita juga bisa melihat masa-masa Hamka sebagai pemimpin redaksi majalah Pedoman Masyarakat. Namun, kehidupan Hamka tak bisa 
+          lepas dari berbagai konflik, termasuk kedatangan Jepang ke Indonesia.
+          </Text>
+        </View>
         <View style={styles.BoxNavbar}>
           <Icon name="home-sharp" size={35} style={{color: "black", left: 20}} ></Icon>
           <Icon name="ios-search" size={35} style={{color: "black", top: -34, left: 120}} ></Icon>
@@ -79,6 +93,11 @@ const DetailPage = ({ navigation }) => {
       top: 120,
       borderRadius: 10,
     },
+    gambar: {
+      width: "100%",
+      height: "100%",
+      borderRadius: 10,
+    },
     judul: {
       position: "absolute",
       color: "white",
@@ -108,7 +127,24 @@ const DetailPage = ({ navigation }) => {
       justifyContent: "center",
       alignItems: "center",
     },
+    BoxJenis2: {
+      position: "absolute",
+      backgroundColor: "#335758",
+      width: 70,
+      height: 25,
+      left: 180,
+      top: 400,
+      borderRadius: 3,
+      justifyContent: "center",
+      alignItems: "center",
+    },
     jenis: {
+      color: "white",
+      fontSize: 15,
+      fontWeight: "bold",
+      textAlign: "center",
+    },
+    jenis2: {
       color: "white",
       fontSize: 15,
       fontWeight: "bold",
@@ -125,7 +161,7 @@ const DetailPage = ({ navigation }) => {
     },
     BoxSinopsis: {
       position: "absolute",
-      backgroundColor: "white",
+      backgroundColor: "#335758",
       width: 350,
       height: 200,
       left: 15,
@@ -142,15 +178,36 @@ const DetailPage = ({ navigation }) => {
       alignItems: "baseline",
     },
     BoxJudul: {
-      backgroundColor: "white",
+      backgroundColor: "#2E4F4F",
       width: 190,
       height: 160,
       left: 180,
-      top: 104,
+      top: 110,
       borderRadius: 10,
     },
-    from:{
+    cast: {
+      color: "white",
       alignContent: "space-between",
+    },
+    from:{
+      color: "white",
+      top: 8,
+      alignContent: "space-between",
+    },
+    rilis: {
+      color: "white",
+      top: 16,
+      alignContent: "space-between",
+    },
+    durasi: {
+      color: "white",
+      top: 24,
+      alignContent: "space-between",
+    },
+    spoiler: {
+      color: "white",
+      top: 10,
+      textAlign: "justify",
     },
 
   });

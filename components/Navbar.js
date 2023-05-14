084cd04c1c609bf9fc02ecd1a3323a8ac23e8f83
rYuuXHikaRi <ryuux.devcode@gmail.com>
2023-05-14 19:17:33 +0700

    Update Navbar.js
    Change Icon,  add IF-ELSE to change color with condition

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,41 +3,57 @@ import {
     View,
     Text,
     StyleSheet,
-    SafeAreaView
+    SafeAreaView,
+    Pressable
 } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
 
-// local
+import Octicons from "react-native-vector-icons/Octicons"
+import Foundation from "react-native-vector-icons/Foundation";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-import SafeViewAndroid from "./SafeViewAndroid";
-
-class Header extends Component {
+class Navbar extends Component {
     render() {
         return (
-            <SafeAreaView style={[SafeViewAndroid.AndroidSafeArea, {backgroundColor: this.props.whichPage === "AboutDev" ? "#CBE4DE" : "#CBE4DE",}]}>
-                <View style={[styles.container, {backgroundColor: "black"}]}>
-                    <Icon name="ios-arrow-back-sharp" size={25} style={{color: "#CBE4DE"}} ></Icon>
-                    <Text style={styles.headerText}>{this.props.headerTitle}</Text>
-                    <View style={styles.dummyItem}></View>   
+                <View style={[styles.container, {backgroundColor: "#CBE4DE"}]}>
+                    <Pressable style={styles.itemNavbar}>
+                        <Foundation name="home" size={30} style={{color: this.props.whichPage === "home" ? "#0E8388" : "#2C3333"}}/>
+                        <Text style={{fontSize: 12, fontWeight:"400", color: this.props.whichPage === "home" ? "#0E8388" : "#2C3333"}}>Home</Text>
+                    </Pressable>
+                    <Pressable style={styles.itemNavbar}>
+                        <Octicons name="search" size={30} style={{color: this.props.whichPage === "search" ? "#0E8388" : "#2C3333"}}/>
+                        <Text style={{fontSize: 12, fontWeight:"400", color:this.props.whichPage === "search" ? "#0E8388" : "#2C3333"}}>Search</Text>
+                    </Pressable>
+                    <Pressable style={styles.itemNavbar}>
+                        <MaterialIcons name="bar-chart" size={30} style={{color: this.props.whichPage === "ranking" ? "#0E8388" : "#2C3333"}}/>
+                        <Text style={{fontSize: 12, fontWeight:"400", color:this.props.whichPage === "ranking" ? "#0E8388" : "#2C3333"}}>Rank</Text>
+                    </Pressable>
+                    <Pressable style={styles.itemNavbar}>
+                        <FontAwesome name="gear" size={30} style={{color: this.props.whichPage === "setting" ? "#0E8388" : "#2C3333"}}/>
+                        <Text style={{fontSize: 12, fontWeight:"400", color: this.props.whichPage === "setting" ? "#0E8388" : "#2C3333"}}>Setting</Text>
+                    </Pressable>
                 </View>
-            </SafeAreaView>
         );
     }
 }
-export default Header;
+export default Navbar;
 
 const styles = StyleSheet.create({
     container: {
+        width: 390,
+        height: 60,
+
         flexDirection : 'row',
         height: 59,
         alignItems : "center",
-        justifyContent: 'space-between',
-        backgroundColor: "#CBE4DE",
+        justifyContent: 'space-around',
 
-        paddingRight: 13,
-        paddingLeft: 13,
+        paddingTop: 8,
+
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
     },
-    headerText:{
+    navbarText:{
         fontStyle: "normal",
         fontWeight: "700",
         fontSize: 16,
@@ -45,8 +61,8 @@ const styles = StyleSheet.create({
 
         alignSelf: "center",
     },
-    dummyItem : {
-        width: 280,
-        height: 30,
+    itemNavbar:{
+        alignItems: "center",
+        justifyContent: "center",
     },
 });
